9e2be0b333babdb3089b6b32d0a4e5a365f35bab
Sum1 <dprssdsumi@gmail.com>
2026-07-26 18:39:00 -0500

    server.js edit

--- server.js
+++ server.js
@@ -36,4 +36,4 @@ app.use(orderRoutes);
 app.use(productRoutes);
 app.use(discountRoutes);
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
